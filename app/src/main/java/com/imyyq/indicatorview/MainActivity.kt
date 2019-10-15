ebfc767c571d65e35a4a8243de95be26ed694d8b
imyyq-iwith <yyq@iwith-smart.com>
2019-10-15 15:50:46 +0800

    完成基本功能

--- app/src/main/java/com/imyyq/indicatorview/MainActivity.kt
+++ app/src/main/java/com/imyyq/indicatorview/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.imyyq.indicatorview
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onMinus(view: View) {
+        indicatorView.mItemCount--
+    }
+
+    fun onAdd(view: View) {
+        indicatorView.mItemCount++
+    }
+
+    fun onSwitch(view: View) {
+        indicatorView.mCurSelect++
+    }
+
+    fun onLeftSwitch(view: View) {
+        indicatorView.mCurSelect--
     }
 }
